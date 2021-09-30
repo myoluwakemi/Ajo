5dd5a11333a4e6b0d76f492d0ed4a8ad5d5edf35
KEMI <yusufoluwakemi39@gmail.comm>
2021-09-30 21:10:29 +0100

    created resuable app button

---
+++ frontend/ajo-frontend/src/components/resusable/App-button.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-btn :loading="loading"
+           :width="width"
+           :height="height"
+           :disabled="disabled"
+           @click="$emit('click')"
+           color="primary"
+    >{{text}} </v-btn>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "App-button",
+  props: {
+    width: {
+      type: String,
+      default: "100%"
+    },
+    height: {
+      type: String,
+      default: "48px"
+    },
+    loading: Boolean,
+    disabled: [Boolean],
+    text: String
+
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
