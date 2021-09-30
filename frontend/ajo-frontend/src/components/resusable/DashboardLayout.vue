bf2c5cb0072f1ae394190c5e6da760d31aca868c
KEMI <yusufoluwakemi39@gmail.comm>
2021-09-30 21:12:58 +0100

    dashboardview

---
+++ frontend/ajo-frontend/src/components/resusable/DashboardLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <slot name="header"></slot>
+      <v-row  no-gutters style="margin-top: 55px" class="pa-2  ">
+        <v-col cols="3" class="side-nav" >
+          <slot name="side-nav"></slot>
+        </v-col>
+        <v-col sm="12" md="9">
+          <div class="dashboard-content pr-4">
+          <slot name="main-content"/>
+          </div>
+        </v-col>
+
+      </v-row>
+    <BottomNav/>
+
+
+  </div>
+
+</template>
+
+<script>
+import BottomNav from "./BottomNav";
+export default {
+  name: "DashboardLayout",
+  components: {BottomNav}
+}
+</script>
+
+<style scoped lang="scss">
+.side-nav{
+  @media (max-width: 768px) {
+    display: none;
+  }
+}
+
+</style>
---
+++ frontend/ajo-frontend/src/views/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <dashboard-layout>
+    <template #side-nav>
+      <DashboardSideNav/>
+    </template>
+    <template #main-content>
+     <router-view/>
+    </template>
+  </dashboard-layout>
+</div>
+</template>
+
+<script>
+import DashboardLayout from "../components/resusable/DashboardLayout";
+import DashboardSideNav from "../components/resusable/DashboardSideNav";
+export default {
+  name: "Dashboard",
+  components: {DashboardSideNav, DashboardLayout}
+}
+</script>
+
+<style scoped>
+
+</style>
