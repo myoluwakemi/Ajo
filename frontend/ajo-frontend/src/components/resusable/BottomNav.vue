4c4e84b1936ec35f80cdd84f294b49bbd422ef1d
KEMI <yusufoluwakemi39@gmail.comm>
2021-09-30 21:11:03 +0100

    bottom nav for responsiveness

---
+++ frontend/ajo-frontend/src/components/resusable/BottomNav.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="button-nav">
+    <v-bottom-navigation v-model="value"  fixed color="#008eef">
+      <v-btn v-for="(data,i ) in menu" :key="i" :value="data.name" :exact="data.exact" :to="data.route">
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: "BottomNav",
+  data(){
+    return{
+      value:'',
+    }
+  },
+  props: {
+    menu:{
+      type: Array,
+      default: function () {
+        return [
+
+          {
+            id: 1,
+            name: "Home",
+            exact: true,
+            icon: history
+
+          },
+
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.button-nav{
+  @media (min-width: 769px) {
+    display: none;
+  }
+}
+
+</style>
