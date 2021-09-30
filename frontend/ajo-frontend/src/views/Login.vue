fee0f8945b4b5c0b5a9ea99d778a88aea047ac9d
KEMI <yusufoluwakemi39@gmail.comm>
2021-09-30 19:37:55 +0100

    login page

---
+++ frontend/ajo-frontend/src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <h3>LOGIN</h3>
+    <div class="mt-6">
+      <h5 class="email">Email Address</h5>
+      <v-text-field placeholder="Enter your email address" type="email" />
+    </div>
+    <div>
+      <h5 class="password">Password</h5>
+      <v-text-field placeholder="Enter your password"
+                    :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="() => (value = !value)"
+                    :type="value ? 'text' : 'password'"
+                    id="password"/>
+
+    </div>
+    <AppButton text="Submit"/>
+
+
+
+  </div>
+</template>
+
+<script>
+  import AppButton from "../components/resusable/App-button";
+  export default {
+    name: 'Login',
+    components: {AppButton},
+    data: ()=>{
+      return{
+        value: false
+      }
+    }
+  }
+</script>
+<style>
+.wrapper{
+  margin: 10rem auto;
+  max-width: 400px;
+  padding: 1rem;
+
+}
+</style>
